b452dc023d0174778024b8a7d94d0b686e224b7f
MohammadBehdad MohammadiDamnab <mohammadbehdadmohammadi@gmail.com>
2026-05-05 14:14:30 -0700

    adding two boot modes

--- BootLoader_ESP32/bootloader_components/main/bootloader_start.c
+++ BootLoader_ESP32/bootloader_components/main/bootloader_start.c
@@ -11,10 +11,21 @@
 #include "bootloader_common.h"
 #include "bootloader_hooks.h"
 
+#include "soc/gpio_reg.h"
+#include "soc/io_mux_reg.h"
+
+
+// Boot mode definitions
+typedef enum {
+    BOOT_MODE_NORMAL = 0,
+    BOOT_MODE_DIAGNOSTICS = 1
+} boot_mode_t;
+
 static const char *TAG = "boot";
 
 static int select_partition_number(bootloader_state_t *bs);
 static int selected_boot_partition(const bootloader_state_t *bs);
+static boot_mode_t detect_boot_mode(void);
 
 /*
  * We arrive here after the ROM bootloader finished loading this second stage bootloader from flash.
@@ -81,6 +92,9 @@ static int select_partition_number(bootloader_state_t *bs)
 static int selected_boot_partition(const bootloader_state_t *bs)
 {
     int boot_index = bootloader_utility_get_selected_boot_partition(bs);
+
+    boot_mode_t mode = detect_boot_mode();
+
     if (boot_index == INVALID_INDEX) {
         return boot_index; // Unrecoverable failure (not due to corrupt ota data or bad partition contents)
     }
@@ -140,3 +154,27 @@ struct _reent *__getreent(void)
     return _GLOBAL_REENT;
 }
 #endif
+
+static boot_mode_t detect_boot_mode(void)
+{
+    // Configure GPIOs as inputs with pullups
+    REG_WRITE(GPIO_ENABLE_W1TC_REG, BIT(23));
+    
+    // Enable pullups
+    REG_SET_BIT(PERIPHS_IO_MUX_GPIO23_U, FUN_PD);
+    
+    // Small delay for pins to settle
+    esp_rom_delay_us(10000);
+    
+    // Read GPIO states
+    uint32_t gpio_state = REG_READ(GPIO_IN_REG);
+    bool gpio23_pressed = (gpio_state & BIT(23));
+    
+    if (gpio23_pressed) {
+        esp_rom_printf("► FACTORY RESET MODE DETECTED\n");
+        return BOOT_MODE_DIAGNOSTICS;
+    }
+    
+    esp_rom_printf("► NORMAL BOOT MODE\n");
+    return BOOT_MODE_NORMAL;
+}
--- BootLoader_ESP32/main/main.c
+++ BootLoader_ESP32/main/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+
+#include "soc/gpio_reg.h"
+
+#define MY_PIN 23
+
 void app_main(void)
 {
     
